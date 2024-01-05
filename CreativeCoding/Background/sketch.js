e6cc7fe2b44934a7b5a9d9953909b1269bc268af
mw4lk3r <malpal512@icloud.com>
2024-01-05 11:01:02 -0500

    background

---
+++ CreativeCoding/Background/sketch.js
@@ -0,0 +1,8 @@
+function setup() {//runs one time
+ createCanvas (windowWidth, windowHeight); //canvas sized to window
+ background ('pink'); //background color white
+}
+
+function draw() {//runs in a loop
+  
+}

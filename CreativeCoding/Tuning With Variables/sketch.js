2b30d4603c9148847f310ec2795472dffb97b811
mw4lk3r <malpal512@icloud.com>
2024-01-08 14:22:39 -0500

    Tuning with Variables

---
+++ CreativeCoding/Tuning With Variables/sketch.js
@@ -0,0 +1,43 @@
+// create a program that enables us to customize the number of squares in a grid
+// two variables. 1) to chose the number of squares, 2) one that defines the side length based on the window width
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  strokeWeight(1);
+  fill ('gray');
+}
+
+function draw() {
+  // runs in a loop
+  
+  var num = 30; // variable for the number of squares
+  var sideLen = windowWidth/num; // variable for the side length of each square
+  
+  
+   for (var y = 0; y < windowHeight; y = y + sideLen) { // loop to create a grid in the y direction
+  
+  for (var x = 0; x < windowWidth; x = x + sideLen) { // loop to create a row of squares in the x direction
+     quad (
+    x,y,
+    x + sideLen,y,
+    x + sideLen,y + sideLen,
+    x,y + sideLen
+  );
+    
+    
+  }
+  
+  quad (
+    0,0,
+    sideLen,0,
+    sideLen,sideLen,
+    0,sideLen
+  );
+
+
+}
+}
+
+function windowResized (){
+  resizeCanvas(windowWidth, windowHeight)
+}

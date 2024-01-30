6aa3628a7abd794c75537d1181c69fab45a6e3a5
mw4lk3r <malpal512@icloud.com>
2024-01-30 08:42:10 -0500

    update sketch.js

--- CreativeCoding/Variables_GIF/sketch.js
+++ CreativeCoding/Variables_GIF/sketch.js
@@ -9,37 +9,30 @@ gif = load Image ('Assets/GIF_Walker copy.gif');
 // two variables. 1) to chose the number of squares, 2) one that defines the side length based on the window width
 
 function setup() {
+	// runs one time
   createCanvas(windowWidth, windowHeight);
+  cursor('assets/explode.png')
+  fill ('gray');
   strokeWeight(1);
-  fill ('white');
 }
 
 function draw() {
   // runs in a loop
   
+  background ('white');
+  
   var num = 5; // variable for the number of squares
   var sideLen = windowWidth/num; // variable for the side length of each square
   
   
-   for (var y = 0; y < windowWidth; y = y + sideLen) { // loop to create a grid in the y direction
+   for (var y = 0; y < 2 * windowHeight; y = y + sideLen) { // loop to create a grid in the y direction
   
-  for (var x = 0; x < windowWidth; x = x + sideLen) { // loop to create a row of squares in the x direction
-    image (gif, x, y, sideLen, sideLen);
-  );
-    
-    
+  for (var x = 0; x < 2 * windowWidth; x = x + sideLen) { // loop to create a row of squares in the x direction
+image (gif, x, y, sideLen, sideLen);
+		}    
+    }
   }
   
-  quad (x, y, 
-    x + sideLen,y,
-    x + sideLen, y + sideLen,
-    sideLen, sideLen,
-    x, y + sideLen);
-
-
-}
-}
-
 function windowResized (){
   resizeCanvas(windowWidth, windowHeight)
 }
